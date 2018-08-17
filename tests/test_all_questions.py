feaceb333ca2d14df518c902a9b3685d2ce2d5e6
bwmurunga <bwmurunga@gmail.com>
2018-08-17 16:46:19 +0300

    Add more tests

--- tests/test_all_questions.py
+++ tests/test_all_questions.py
@@ -3,8 +3,39 @@ from api.v1.views import get_tasks
 
 
 def test_get_all_questions(client):
+    expected_result = {
+    "tasks": [
+        {
+            "description": "Milk, Cheese, Pizza, Fruit, Tylenol",
+            "done": False,
+            "id": 1,
+            "title": "Buy groceries"
+        },
+        {
+            "description": "Need to find a good Python tutorial on the web",
+            "done": False,
+            "id": 2,
+            "title": "Learn Python"
+        }
+    ]
+}
     response = client.get('/todo/api/v1.0/tasks')
     assert response.status_code == 200
     assert b'"id":1' in response.data
     assert b'"id":2' in response.data
+    assert expected_result == response.get_json()
+
+def test_post_question(client):
+    expected_result = {
+    "task": {
+        "description": "",
+        "done": False,
+        "id": 3,
+        "title": "Read a book"
+        }
+    }
+    response = client.post('/todo/api/v1.0/tasks', json={"title":"Read a book"})
+    assert response.status_code == 201
+    assert expected_result == response.get_json()
+
 
